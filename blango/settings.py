cd16e5f6588d946b731fb4491f50646869d5ae76
Kan Igor <ikanv13@gmail.com>
2024-05-14 06:26:45 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -59,6 +59,7 @@ class Dev(Configuration):
         'blog',
         "crispy_forms",
         "crispy_bootstrap5",
+        "rest_framework",
         "debug_toolbar",
         "allauth",
         "allauth.account",
