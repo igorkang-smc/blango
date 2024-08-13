3fe36e503ca82247aec93d4610b5aa599276aa1c
Kan Igor <ikanv13@gmail.com>
2024-08-13 06:41:36 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
     name="django_registration_register",
 ),
 path("accounts/", include("django_registration.backends.activation.urls")),
-path("api/v1/", include("blog.api.urls"))
+path("api/v1/", include("blog.api.urls")),
+path("post-table/", blog.views.post_table, name="blog-post-table"),
 ]
